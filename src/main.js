f55b1793e6764aa473d1f5d56c9dfcb49810e4be
U-EMERGEDESK\Franck <franckbinard@gmail.com>
2019-11-13 22:19:43 -0500

    basic threejs setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 "use strict"
-
+const THREE = require('three')
 
 $(function(){
+    let scene, camera, container, renderer, geometry, material, mesh
 
-    $("p").text('fuck you')
+    container = document.querySelector('#scene-container')
 
+    camera = new THREE.PerspectiveCamera( 35, container.clientWidth / container.clientHeight, 0.1, 100)
+    camera.position.set( 0, 0, 10)
+
+    geometry = new THREE.BoxBufferGeometry(2, 2, 2)
+    material = new THREE.MeshBasicMaterial()
+    mesh = new THREE.Mesh( geometry, material ) 
+
+    scene = new THREE.Scene()
+    scene.background = new THREE.Color( 'skyblue' )
+    scene.add( mesh )
+    renderer = new THREE.WebGLRenderer( {antialias: true} )
+    renderer.setSize( container.clientWidth, container.clientHeight)
+    renderer.setPixelRatio( window.devicePixelRatio ) 
+
+    container.appendChild( renderer.domElement )
+    renderer.render( scene, camera )
 }); 
