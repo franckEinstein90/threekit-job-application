2a4c98dd85161522bcd710b916f3139571aba7c3
Franck <franckbinard@gmail.com>
2019-11-15 15:55:45 -0500

    threejs plane

--- src/main.js
+++ src/main.js
@@ -1,21 +1,34 @@
 "use strict"
 const THREE = require('three')
 
+
+let initGrid = function(scene){
+    let gridHelper = new THREE.GridHelper(1000, 200)
+    scene.add(gridHelper)
+}
+let initLights = function(scene){
+    let ambientLight = new THREE.AmbientLight( 0x606060 )
+    scene.add( ambientLight )
+    let directionalLight = new THREE.DirectionalLight(0xffffff)
+    scene.add( directionalLight)
+}
+
 $(function(){
     let scene, camera, container, renderer, geometry, material, mesh
 
     container = document.querySelector('#scene-container')
 
-    camera = new THREE.PerspectiveCamera( 35, container.clientWidth / container.clientHeight, 0.1, 100)
-    camera.position.set( 0, 0, 10)
+    camera = new THREE.PerspectiveCamera( 45, container.clientWidth / container.clientHeight, 1, 10000)
+    camera.position.set( 200, 800, 1300)
+    camera.lookAt(0,0,0)
 
-    geometry = new THREE.BoxBufferGeometry(2, 2, 2)
-    material = new THREE.MeshBasicMaterial()
-    mesh = new THREE.Mesh( geometry, material ) 
 
     scene = new THREE.Scene()
     scene.background = new THREE.Color( 'skyblue' )
-    scene.add( mesh )
+
+    initGrid(scene)
+    initLights(scene)
+
     renderer = new THREE.WebGLRenderer( {antialias: true} )
     renderer.setSize( container.clientWidth, container.clientHeight)
     renderer.setPixelRatio( window.devicePixelRatio ) 
